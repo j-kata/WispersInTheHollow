4daba8c740ff9d3b61611d1a3b9288c9abc8fa60
katja tsareva <katja.tcareva@gmail.com>
2025-06-04 12:09:51 +0200

    refactor parser

--- WispersInTheHollow/Extensions/StringExtensions.cs
+++ WispersInTheHollow/Extensions/StringExtensions.cs
@@ -1,7 +1,7 @@
 namespace WispersInTheHollow.Extensions;
 
 internal static class StringExtensions {
-    public static string[] SplitToLower(this string str, string separator)
+    public static string[] SplitTrimToLower(this string str, string separator)
     {
         string[] split = str.Split(separator, StringSplitOptions.RemoveEmptyEntries);
         return [.. split.Select(s => s.ToLower())];
--- WispersInTheHollow/Helpers/CommandParser.cs
+++ WispersInTheHollow/Helpers/CommandParser.cs
@@ -5,30 +5,25 @@ namespace WispersInTheHollow.Helpers;
 
 internal static class CommandParser
 {
+  private static readonly Dictionary<string, Func<string[], ICommand>> CommandMap = new()
+  {
+    { "go", args => args.Length > 0 ? new MoveCommand(args[0]) : new InvalidCommand() },
+    { "inspect", args => new InspectCommand(string.Join(" ", args)) },
+    { "help", args => new HelperCommand() }
+  };
+  
   public static ICommand Parse(string input)
   {
     var (command, arguments) = SplitInput(input);
     if (command == null) return new InvalidCommand();
 
-    switch (command)
-    {
-      case "go":
-        var direction = arguments.Take(1).FirstOrDefault();
-        return direction == null ? new InvalidCommand() : new MoveCommand(direction);
-      case "look":
-      case "search":
-      case "inspect":
-        return new InspectCommand(string.Join(" ", arguments));
-      case "help":
-        return new HelperCommand();
-      default:
-        return new InvalidCommand();
-    }
+    //command = CommandAliases.TryGetValue(command, out var realCmd) ? realCmd : command;
+    return CommandMap.TryGetValue(command, out var factory) ? factory(arguments) : new InvalidCommand();
   }
 
-  private static (string? command, IEnumerable<string> arguments) SplitInput(string input)
+  private static (string? command, string[] arguments) SplitInput(string input)
   {
-    string[] data = input.SplitToLower(" ");
-    return (data.Take(1).FirstOrDefault(), data.Skip(1));
+    string[] data = input.SplitTrimToLower(" ");
+    return (data.FirstOrDefault(), data.Skip(1).ToArray());
   }
 }
